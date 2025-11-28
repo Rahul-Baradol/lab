9108a2fa87ba5b9627a8e729afb033f5c606035e
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-11-28 22:25:02 +0530

    home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='w-screen h-screen flex justify-center items-center bg-black '>
+      <div className='text-md italic text-white'>Not all who wander, are lost :)</div>
+    </div>
   )
 }
 
